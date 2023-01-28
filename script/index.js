3d8dd889d16f3c94323506c83d14abb81e2801cf
d.8oulevard <d.8oulevard@yandex.ru>
2023-01-28 17:12:53 +0300

    добавил закрытие попапов

--- script/index.js
+++ script/index.js
@@ -1,38 +1,3 @@
-// const popup = document.querySelector('.popup')
-// const editPopupOpenBtn = document.querySelector('.profile__edit-button')
-// const editPopupCloseBtn = document.querySelector('.popup__close-button')
-// const form = document.querySelector('.popup__form')
-// const formInputName = document.querySelector('.popup__input_type_name')
-// const formInputDescription = document.querySelector('.popup__input_type_description')
-
-// function handleFormSubmit(e) {
-//   e.preventDefault();
-
-//   document.querySelector('.profile__name').textContent = formInputName.value
-//   document.querySelector('.profile__description').textContent = formInputDescription.value
-
-//   popup.classList.remove('popup_active')
-// }
-
-// function openPopup() {
-//   popup.classList.add('popup_active')
-
-//   const profileName = document.querySelector('.profile__name')
-//   const profileDescription = document.querySelector('.profile__description')
-
-//   formInputName.value = profileName.textContent
-//   formInputDescription.value = profileDescription.textContent
-// }
-
-// function closePopup(e) {
-//   popup.classList.remove('popup_active')
-// }
-
-
-// editPopupOpenBtn.addEventListener('click', openPopup)
-// editPopupCloseBtn.addEventListener('click', closePopup)
-// form.addEventListener('submit', handleFormSubmit)
-
 const initialCards = [
   {
     name: 'Архыз',
@@ -81,18 +46,37 @@ const imagePopupDescription = imagePopup.querySelector('.popup__description')
 const profileName = document.querySelector('.profile__name')
 const profileDescription = document.querySelector('.profile__description')
 
+const popupList = Array.from(document.querySelectorAll('.popup'))
+
+popupList.forEach(el => {
+  el.addEventListener('click', e => {
+    if (e.target.classList.contains('popup_active')) {
+      closePopup(el)
+    }
+  })
+})
+
+function closePopupPressEsc(e) {
+  if (e.key === "Escape") {
+    const openEl = document.querySelector('.popup_active')
+    closePopup(openEl)
+  }
+
+}
+
 function openPopup(popupType) {
   popupType.classList.add('popup_active')
+  document.addEventListener('keydown', closePopupPressEsc)
+}
+
+function closePopup(popupType) {
+  popupType.classList.remove('popup_active')
 }
 
 function addDefaultPopupValue(popupType) {
   openPopup(popupType)
   editInputName.value = profileName.textContent
   editInputDescription.value = profileDescription.textContent
-}
-
-function closePopup(popupType) {
-  popupType.classList.remove('popup_active')
 }
 
 function handleFormSubmit(e) {
